9750081b1dc9a27cc890ff015dc089e75f0c3d6b
Samiul Islam <samiulislamsharan@gmail.com>
2025-03-12 03:18:04 +0600

    add scroll-based Lottie animation for lines in index.html and interactions.js

---
+++ js/interactions.js
@@ -0,0 +1,11 @@
+LottieInteractivity.create({
+    player: '#lottie-scroll-lines',
+    mode: 'scroll',
+    actions: [
+        {
+            visibility: [0, 1],
+            type: 'seek',
+            frames: [90, 123],
+        },
+    ]
+});
