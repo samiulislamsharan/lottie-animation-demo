2ce3313ed9b971dc71102831f62b658f0f6e34b8
Samiul Islam <samiulislamsharan@gmail.com>
2025-03-12 03:18:28 +0600

    add interactive Lottie animation for blueprint with cursor-based interaction in index.html and interactions.js

--- js/interactions.js
+++ js/interactions.js
@@ -9,3 +9,15 @@ LottieInteractivity.create({
         },
     ]
 });
+
+LottieInteractivity.create({
+    player: '#lottie-sync-cursor-blueprint',
+    mode: 'cursor',
+    actions: [
+        {
+            position: { x: [0, 1], y: [0, 1] },
+            type: "seek",
+            frames: [0, 42]
+        }
+    ]
+});
